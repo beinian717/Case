c7f974c39854273fff8001e870d8ef518a21a502
songzhikun <songzhikun@spatialinfo.cn>
2024-02-11 10:56:09 +0800

    增加注释

--- Case/index.js
+++ Case/index.js
@@ -1,44 +1,48 @@
-class CompressImg{
-    
-    constructor(options){
-        this.options=options;
+class CompressImg {
+    constructor(options) {
+        this.options = options;
         this.createBase64();
     }
-    createBase64(){
-        // 92.8kb
-        const fileReader=new FileReader();
+    // 将文件流转换成base64格式
+    createBase64() {
+        // 创建文件读取器
+        const fileReader = new FileReader();
         fileReader.readAsDataURL(this.options.file)
-        fileReader.onload=(e)=>{
-           this.compress(e.target.result)
+        fileReader.onload = (e) => {
+            // 对转换后的base64进行压缩
+            this.compress(e.target.result)
         }
     }
-    compress(base64){
-        const img=new Image();
-        const canvas=document.createElement('canvas');
-        const ctx=canvas.getContext('2d');
-        img.src=base64;
-        img.onload=()=>{
-            canvas.width=img.width;
-            canvas.height=img.height;
-            ctx.drawImage(img,0,0,img.width,img.height);
-            const url=canvas.toDataURL(this.options.file.type,this.options.quality);
+    compress(base64) {
+        const img = new Image();
+        const canvas = document.createElement('canvas');
+        // 定义画笔
+        const ctx = canvas.getContext('2d');
+        // 为了获取选择图片的宽高(即原始样式)
+        img.src = base64;
+        img.onload = () => {
+            canvas.width = img.width;
+            canvas.height = img.height;
+            // drawImage(图片,x,y,width,height)
+            ctx.drawImage(img, 0, 0, img.width, img.height);
+            // 自定义图片的质量,用于将canvas对象转为base64编码格式
+            const url = canvas.toDataURL(this.options.file.type, this.options.quality);
             this.options.success(url);
         }
     }
 }
 
 const ipt = document.querySelector("#ipt");
-ipt.addEventListener("change",(e)=>{
+ipt.addEventListener("change", (e) => {
     const fileObj = e.target.files[0];
-    if(fileObj){
+    if (fileObj) {
         new CompressImg({
-            file:fileObj,
-            quality:0.1,
-            success(url){
-                console.log(url)
-                const img=new Image();
-                img.src=url;
-                img.onload=function(){
+            file: fileObj,
+            quality: 0.1,
+            success(url) {
+                const img = new Image();
+                img.src = url;
+                img.onload = function () {
                     document.body.appendChild(img)
                 }
             }
